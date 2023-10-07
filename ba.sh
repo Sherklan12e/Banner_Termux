0c8db6bf52562f4a56189fc0fa4eb325aed19684
Sherklan12e <davismendoza.08@gmail.com>
2023-10-07 19:01:45 -0300

    dsf

--- ba.sh
+++ ba.sh
@@ -72,7 +72,6 @@ while true; do
             contenido="
             import ascii_wizard
             import os
-
             img = ascii_wizard.AsciiWizard(\"$imagen\")
             img.colored(50, 2.2, None, None, True)
             "
